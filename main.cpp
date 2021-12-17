d4d6dc92ab077ae1df6a343ef63dabf55eb9b2e3
Alst0wn <bugrova.ad@phystech.edu>
2021-12-17 23:06:49 +0300

    vector structure

--- main.cpp
+++ main.cpp
@@ -1,24 +1,38 @@
-vector<double> f (double a, double b, double c, double d, double e, vector<double> calc) {//функция в диффуре
-    vector<double> ret;
-    ret.push_back()
-    return ret
+#include <cmath>
+#include <iostream>
+
+struct vec {
+    double x, y, p, t;
+    vec(double x, double y, double p, double t): x(x), y(y), p(p), t(t){}
+    vec operator + (const vec &s) {return vec(x+s.x, y+s.y, p+s.p, t+s.t);}
+    vec operator * (double c) {return vec(c*x, c*y, c*p, c*t);}
 }
 
 
-vector<double> RKstep (double a, double b, double c, double d, double e, vector<double> start) { //шаг метода Рунге-Кутты
-    vector<double> k1, k2, k3, k4;
+
+
+vec f (double a, double b, double c, double d, double e, vec calc) {//функция в диффуре
+    vec ret;
+    return vec(,,,);
+}
+
+
+vec RKstep (double a, double b, double c, double d, double e, vec start) { //шаг метода Рунге-Кутты
+    vec k1, k2, k3, k4;
     const double stepsize = 0.0000001;
     k1 = f(a, b, c, d, e, start);
-    k2 = f(a, b, c, d, e, start+stepsize*k1/2);
-    k3 = f(a, b, c, d, e, start+stepsize*k2/2);
-    k4 = f(a, b, c, d, e, start+stepsize*k3);
-    return start+stepsize*(k1+2*k2+2*k3+k4)/6;
+    k2 = f(a, b, c, d, e, start+k1*(0.5)*stepsize);
+    k3 = f(a, b, c, d, e, start+k2*(0.5)*stepsize;
+    k4 = f(a, b, c, d, e, start+k3*stepsize);
+    return start+(k1+k2*2+k3*2+k4)*(double(1)/double(6))*stepsize;
 }
 
-void render (double L, double l, vector<double> position) {
+void render (double L, double l, vec position) {
 
 }
 
 int main() {
- cin >> L >> l >> M >> m;
+    double L, l, M, m, a, b, c, d, e;
+    cin >> L >> l >> M >> m;
+    vector<double> position;
 }
