b0683eceaeb46374ed935ab7e5f8a8e5380dfd1c
Alst0wn <bugrova.ad@phystech.edu>
2021-12-18 00:02:06 +0300

    differation equation function

--- main.cpp
+++ main.cpp
@@ -11,9 +11,8 @@ struct vec {
 
 
 
-vec f (double a, double b, double c, double d, double e, vec calc) {//функция в диффуре
-    vec ret;
-    return vec(,,,);
+vec f (double a, double b, double c, double d, double e, vec v) {//функция в диффуре
+    return vec((e*sin(v.t)-2*b*sin(v.t-v.p)*(cos(v.t-v.p)*b/2/a*v.x*v.x+v.y*v.y)-b*d/2/a*sin(v.p)*cos(v.t-v.p))/(2*c-b*b*cos(v.t-v.p)*cos(v.t-v.p)/2/a), (d*sin(v.p)+2*b*sin(v.t-v.p)*(cos(v.t-v.p)*b/2/c*v.y*v.y+v.x*v.x)-b*e/2/c*sin(v.t)*cos(v.t-v.p))/(2*a-b*b*cos(v.t-v.p)*cos(v.t-v.p)/2/c), y, x);
 }
 
 
@@ -22,7 +21,7 @@ vec RKstep (double a, double b, double c, double d, double e, vec start) { //шаг
     const double stepsize = 0.0000001;
     k1 = f(a, b, c, d, e, start);
     k2 = f(a, b, c, d, e, start+k1*(0.5)*stepsize);
-    k3 = f(a, b, c, d, e, start+k2*(0.5)*stepsize;
+    k3 = f(a, b, c, d, e, start+k2*(0.5)*stepsize);
     k4 = f(a, b, c, d, e, start+k3*stepsize);
     return start+(k1+k2*2+k3*2+k4)*(double(1)/double(6))*stepsize;
 }
@@ -33,6 +32,12 @@ void render (double L, double l, vec position) {
 
 int main() {
     double L, l, M, m, a, b, c, d, e;
+    const double g = 9.81;
     cin >> L >> l >> M >> m;
-    vector<double> position;
+    vec pos;
+    a = M*L*L/6+m*L*L/2;
+    b = m*l*L/4;
+    c = m*l*l/6;
+    d = (M/2+m)*g*L;
+    e = m*g*l/2;
 }
