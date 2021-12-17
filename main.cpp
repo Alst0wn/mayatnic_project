3ca4375b2e4cd74903143fb3f5cb4f32e4c5e214
Alst0wn <bugrova.ad@phystech.edu>
2021-12-17 18:52:23 +0300

    Runge-Kutt step function

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+vector<double> f (double a, double b, double c, double d, double e, vector<double> calc) {//функция в диффуре
+    vector<double> ret;
+    ret.push_back()
+    return ret
+}
 
+
+vector<double> RKstep (double a, double b, double c, double d, double e, vector<double> start) { //шаг метода Рунге-Кутты
+    vector<double> k1, k2, k3, k4;
+    const double stepsize = 0.0000001;
+    k1 = f(a, b, c, d, e, start);
+    k2 = f(a, b, c, d, e, start+stepsize*k1/2);
+    k3 = f(a, b, c, d, e, start+stepsize*k2/2);
+    k4 = f(a, b, c, d, e, start+stepsize*k3);
+    return start+stepsize*(k1+2*k2+2*k3+k4)/6;
+}
+
+void render (double L, double l, vector<double> position) {
+
+}
+
+int main() {
+ cin >> L >> l >> M >> m;
+}
